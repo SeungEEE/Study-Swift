f2582396c3f8a8820aaf3a9de082cc5f74f9bba9
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-03-17 20:27:17 +0900

    iOS Study
    Day 1

---
+++ Development/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 이승진 on 2023/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
