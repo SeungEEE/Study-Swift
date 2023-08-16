d3a962f893977fdb4e2dd66dbdd2f15a073be7fd
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-08-16 12:57:35 +0900

    02_data_type
    기본 데이터 타입

---
+++ course01/02_data_type.playground/Contents.swift
@@ -0,0 +1,44 @@
+/* 데이터 타입 */
+import UIKit
+
+// Swift의 기본 데이터 타입
+
+// Bool
+var someBool: Bool = true
+someBool = false
+// someBool = 0
+// someBool = 1
+
+// Int
+var someInt: Int = -100
+//someInt = 100.1
+
+// UInt(양의 정수)
+var someUInt: UInt = 100
+//someUInt = -100
+//someUInt = someInt
+
+// Float
+var someFloat: Float = 3.14
+someFloat = 3
+
+// Double
+var someDouble: Double = 3.14
+someDouble = 3
+//someDouble = someFloat
+
+// Character
+var someCharacter: Character = "🥳"
+//someCharacter = "🥹"
+//someCharacter = "가"
+//someCharacter = "A"
+//someCharacter = "하하하"
+//print(someCharacter)
+
+// String
+var someString: String = "하하하 😄"
+someString = someString + "웃으면 복이와요"
+//print(someString)
+
+//someString = someCharacter
+
